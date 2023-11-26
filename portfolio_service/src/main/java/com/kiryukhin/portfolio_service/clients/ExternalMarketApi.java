2d2865134947a128fea06ebe4d9a468602faf779
Roman Kiryukhin <rkiryukhin@gmail.com>
2023-11-26 17:32:18 +0700

    Edited URI and params in the request to the market

--- portfolio_service/src/main/java/com/kiryukhin/portfolio_service/clients/ExternalMarketApi.java
+++ portfolio_service/src/main/java/com/kiryukhin/portfolio_service/clients/ExternalMarketApi.java
@@ -3,11 +3,11 @@ package com.kiryukhin.portfolio_service.clients;
 import com.kiryukhin.portfolio_service.clients.model.MarketApiStockResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "market-service")
 public interface ExternalMarketApi {
 
-    @GetMapping
-    MarketApiStockResponse getMarket(@RequestParam String ticker);
+    @GetMapping("/getByTicker/{ticker}")
+    MarketApiStockResponse getMarket(@PathVariable String ticker);
 }
